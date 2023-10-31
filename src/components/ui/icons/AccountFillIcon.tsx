3bd459aac205f94a28f830bd9e1f34022bed7556
kwngmin <kwngmin.k@gmail.com>
2023-10-31 23:18:31 +0900

    불필요한 아이콘 삭제

--- src/components/ui/icons/AccountFillIcon.tsx
+++
@@ -1,5 +0,0 @@
-export default function AccountFillIcon() {
-  return (
-    <span className='material-symbols-rounded fillIcon'>account_circle</span>
-  );
-}
--- src/components/ui/icons/AccountIcon.tsx
+++
@@ -1,3 +0,0 @@
-export default function AccountIcon() {
-  return <span className='material-symbols-rounded'>add_circle</span>;
-}
