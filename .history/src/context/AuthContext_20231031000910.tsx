f26ec13d62163f247ce6ae578f22279a0efa577e
kwngmin <kwngmin.k@gmail.com>
2023-11-05 23:09:11 +0900

    페이지 이동시 스크롤 최상단에서 로드

--- .history/src/context/AuthContext_20231031000910.tsx
+++ .history/src/context/AuthContext_20231031000910.tsx
@@ -1,14 +1,9 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
-import { usePathname } from 'next/navigation';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
 type Props = {
   children: ReactNode;
 };
 export default function AuthContext({ children }: Props) {
-  const pathName = usePathname();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathName]);
   return <SessionProvider>{children}</SessionProvider>;
 }
