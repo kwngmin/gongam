161658049060051b22f6b77e97d70ca0e3db67e7
kwngmin <kwngmin.k@gmail.com>
2023-11-05 22:07:47 +0900

    title font-size 줄이고 navbar의 cancel bg-color 지정

--- .history/src/components/ui/Title_20231030223202.tsx
+++ .history/src/components/ui/Title_20231030223202.tsx
@@ -6,7 +6,7 @@ export default function Title({ titleData }: { titleData: TitleData }) {
   const { title, description } = titleData;
   return (
     <div className='pt-14 pb-16'>
-      <h1 className='text-2xl md:text-3xl font-bold mb-2'>{title}</h1>
+      <h1 className='text-3xl md:text-4xl font-bold mb-2'>{title}</h1>
       <p className='text-black/50 break-keep'>{description}</p>
     </div>
   );
