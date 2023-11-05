303ddb347d2496727bd24fd44e9e59fe2faea7c6
kwngmin <kwngmin.k@gmail.com>
2023-11-05 23:29:39 +0900

    usseeffect 다시 설정

--- .history/src/app/inbox/page_20231105232904.tsx
+++ .history/src/app/inbox/page_20231105232904.tsx
@@ -3,12 +3,11 @@ import Title from '@/components/ui/Title';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
-export default async function BookmarksPage() {
+export default async function InboxPage() {
   const session = await getServerSession(authOptions);
   const titleData = {
-    title: `Bookmarks`,
+    title: `Inbox`,
     description: `Record of thought, moments, feelings that I don't want to forget.`,
   };
   if (!session) {
@@ -20,16 +19,7 @@ export default async function BookmarksPage() {
   return (
     <section className='max-w-screen-md mx-auto px-4 h-fit pb-10'>
       <Title titleData={titleData} />
-      <div className='flex flex-col'>
-        <NotePost />
-        <NotePost />
-        <NotePost />
-        <NotePost />
-        <NotePost />
-        <NotePost />
-        <NotePost />
-        <NotePost />
-      </div>
+      <NotePost />
     </section>
   );
 }
