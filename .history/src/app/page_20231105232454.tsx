339bd98ac3d46cca390207ac1a17ffd4952a631c
kwngmin <kwngmin.k@gmail.com>
2023-11-05 23:27:49 +0900

    scrollto useeffect 제거

---
+++ .history/src/app/page_20231105232454.tsx
@@ -0,0 +1,17 @@
+import NotePost from '@/components/NotePost';
+import Title from '@/components/ui/Title';
+
+export default function Home() {
+  scrollTo(0, 0);
+
+  const titleData = {
+    title: `Notes`,
+    description: `Record of thought, moments, feelings that I don't want to forget.`,
+  };
+  return (
+    <section className='max-w-screen-md mx-auto px-4 h-fit pb-10'>
+      <Title titleData={titleData} />
+      <NotePost />
+    </section>
+  );
+}
---
+++ .history/src/app/page_20231105232731.tsx
@@ -0,0 +1,15 @@
+import NotePost from '@/components/NotePost';
+import Title from '@/components/ui/Title';
+
+export default function Home() {
+  const titleData = {
+    title: `Notes`,
+    description: `Record of thought, moments, feelings that I don't want to forget.`,
+  };
+  return (
+    <section className='max-w-screen-md mx-auto px-4 h-fit pb-10'>
+      <Title titleData={titleData} />
+      <NotePost />
+    </section>
+  );
+}
